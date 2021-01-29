a087a2992320663f849edc137c35c56e3b134d89
Finn Stockwell <2464936S@student.gla.ac.uk>
2021-01-29 10:08:29 +0000

    chapter 5 commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.db import models
+from rango.models import Category, Page
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','category','url')
+    
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+
+
+
