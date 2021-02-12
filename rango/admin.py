34c41eb7072cf110e8fa07226513cd39d34d25d6
Finn Stockwell <2464936S@student.gla.ac.uk>
2021-02-12 15:41:56 +0000

    chapter 9 commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.db import models
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category','url')
@@ -11,6 +11,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
 
 
 
